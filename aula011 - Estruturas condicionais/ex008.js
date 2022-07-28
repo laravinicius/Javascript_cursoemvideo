0fcd6a9436f550a314347038081f6ce5b7920a70
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-28 14:31:08 -0300

    exercícios com if

---
+++ aula011 - Estruturas condicionais/ex008.js
@@ -0,0 +1,6 @@
+let vel = 59;
+console.log(`A velocidade do seu carro é ${vel} KM/h.`);
+if (vel > 60) { // condição simples
+    console.log(`Voce ultrapassou a velocidade permitita. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança.`);
---
+++ aula011 - Estruturas condicionais/ex009.js
@@ -0,0 +1,8 @@
+let pais = `Brasil`;
+
+console.log(`Vivendo em ${pais}`);
+if (pais == `Brasil`){
+    console.log(`Voce é Brasileiro`);
+} else {
+    console.log(`Voce é Estrangeiro`);
+}
