3ccb10a2e1bf9b27a84a4b741108dfddd7449165
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-08-10 08:52:45 -0300

    finalizar projeto contagem

--- ex aula014/ex016/script.js
+++ ex aula014/ex016/script.js
@@ -1,12 +1,35 @@
 function contar(){
     let ini = document.getElementById(`iInicio`);
-    let fim = document.getElementById(`iFIm`);
-    let pas = document.getElementById(`iPassoi`);
+    let fim = document.getElementById(`iFim`);
+    let pas = document.getElementById(`iPasso`);
+    let res = document.getElementById(`iRes`);
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){ // teste se os valores estão vazios
         alert(`[ERRO]Verfique os dados e complete corretamente`);
+        res.innerHTML = `Impossível contar`;
     } else {
-        alert(`Tudo ok`);
+        res.innerHTML = `Contando... <br>`;
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(pas.value);
+
+        if(p <= 0){
+            alert(`Passo inválido, considerando passo 1.`);
+            p = 1;
+        }
+
+        if(i < f){ //contagem cescente
+            for (let c = i; c <= f; c+= p){
+                res.innerHTML += ` \u{27A1} ${c} `;
+            }
+        } else { // contagem decrescente
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `\u{27A1} ${c}`;
+            }
+        }
+        
+        res.innerHTML += `\u{27A1} \u{1F3C1}`;
+
     }
 
 }
